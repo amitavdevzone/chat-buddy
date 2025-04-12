9ea28e9fd266fd40a8b5854e421d6fe1668d538b
Amitav Roy <reachme@amitavroy.com>
2025-04-12 11:34:51 +0530

    Moved the code to save a message now inside the controller.

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\SenderType;
 use App\Models\Conversation;
+use App\Models\Message;
 use App\Services\AiBot\AiBotInterface;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -13,7 +16,7 @@ class ConversationController extends Controller
     {
         $models = $aiBot->getModels();
         $tools = ['Web Search', 'Research'];
-        $defaultModel = 'GPT-3.5';
+        $defaultModel = $models[0];
 
         $conversation = Conversation::query()
             ->with(['messages' => function ($query) {
@@ -27,6 +30,31 @@ class ConversationController extends Controller
             'tools' => $tools,
             'defaultModel' => $defaultModel,
             'conversation' => $conversation,
+        ]);
+    }
+
+    public function store(Request $request, AiBotInterface $aiBot)
+    {
+        $data = $request->validate([
+            'model' => 'required|string',
+            'message' => 'required|string',
+            'tool' => 'nullable|string',
+        ]);
+
+        Message::create([
+            'conversation_id' => 1,
+            'user_id' => auth()->user()->id,
+            'sender_type' => SenderType::USER->value,
+            'message' => $data['message'],
+        ]);
+
+        $response = $aiBot->getCompletion($data['model'], $data['message']);
+
+        Message::create([
+            'conversation_id' => 1,
+            'user_id' => auth()->user()->id,
+            'sender_type' => SenderType::AGENT->value,
+            'message' => $response['message'],
         ]);
     }
 }
